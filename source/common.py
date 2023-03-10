ea3be30c8fd253eee570bcb561704119186c088e
Ali El-Said <palindali@gmail.com>
2023-03-10 13:49:49 +0000

    modify scripts for run

--- source/common.py
+++ source/common.py
@@ -4,6 +4,7 @@ import pandas as pd
 import torch.nn as nn
 from transformers import AutoModelWithLMHead, AutoTokenizer
 
+import pdb
 
 def init_model(model_name: str, device, do_lower_case: bool = False, args=None):
     """
@@ -42,10 +43,11 @@ def load_data(in_file, task="in_shuf"):
     with open(in_file, "r", encoding="utf-8") as f:
         for line in f:
             all_lines.append(json.loads(line))
+    # pdb.set_trace()
     if task == "index_with_sep":
         examples = [
             (
-                f"[shuffled] {' '.join([' '.join((f'<S{i}>', sent)) for i, sent in zip(list(range(len(line['orig_sents']))), line['shuf_sents'])])} [orig]",
+                f"[shuffled] {' '.join([' '.join((f'<S{i}>', sent)) for i, sent in zip(list(range(len(line['shuf_sents']))), line['shuf_sents'])])} [orig]",
                 f"{' '.join(line['orig_sents'])} <eos>",
             )
         for line in all_lines
--- source/format_preds.py
+++ source/format_preds.py
@@ -1,10 +1,22 @@
 # Format the predictions
 import json
+import pandas as pd
+import pdb
 
+# Read output jsonl file
 in_file = './outputs/bart-large_dc2/test_bart_greedy.jsonl'
 all_lines = []
 with open(in_file, "r", encoding="utf-8") as f:
     for line in f:
         all_lines.append(json.loads(line))
 
-orders = [line['predictions'] for line in all_lines]
+# Extract prediction orders
+# pdb.set_trace()
+orders = [[int(v) for v in line['predictions'][0][:-6].split(' ')] for line in all_lines]
+
+# Save orders dataframe in DC format
+orders_df = pd.DataFrame(orders)
+orders_df.columns = ['index_1', 'index_2', 'index_3', 'index_4', 'index_5']
+orders_df['id'] = range(len(orders_df))
+orders_df = orders_df[['id','index_1', 'index_2', 'index_3', 'index_4', 'index_5']]
+orders_df.to_csv(in_file[:-6] + "_orders.csv", index=False)
